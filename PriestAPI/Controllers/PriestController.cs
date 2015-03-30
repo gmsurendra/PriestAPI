40aca4bbd4d7701fc92fcc0c8f6a90990b113a5c
gm.surendra <gm_surendra@hotmail.com>
2015-03-30 05:33:21 +0530

    added priest api

---
+++ PriestAPI/Controllers/PriestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PriestAPI.Controllers
+{
+    public class PriestController : ApiController
+    {
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
